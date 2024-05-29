8d5a3dd392ced4649152a901a59e7e7c8bbbd8df
jumellet <38491251+jumellet@users.noreply.github.com>
2024-05-30 00:15:52 +0200

    Update hatpic-ros1.py
    Connect to mavros to send mavros positions

--- scripts/hatpic-ros1.py
+++ scripts/hatpic-ros1.py
@@ -14,82 +14,151 @@ __maintainer__ = "Julien Mellet"
 __email__ = "julien.mellet@unina.it"
 __status__ = "Production"
 
+import rospy
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import WrenchStamped, PoseStamped, Point
+from mavros_msgs.msg import PositionTarget, AttitudeTarget, State
+import numpy as np
 import serial
 import time
 
-hatpic = serial.Serial(port='/dev/ttyUSB0',  baudrate=115200, timeout=1)
+class Hatpic:
+    def __init__(self):
+        self.hatpic_ser = serial.Serial(port='/dev/ttyUSB0',  baudrate=115200, timeout=1)
 
-data_a = 0
-data_b = 0
-data_c = 0
-data_d = 0
+        # Data from the Hatpic device
+        self.data_a = 0
+        self.data_b = 0
+        self.data_c = 0
+        self.data_d = 0
 
-def write(x):
-    hatpic.write(bytes(x,'utf-8'))
-    time.sleep(0.05)
+        # Coefficient of the joystick
+        self.k_j = 0.00033
+        self.dead_zone = 22
 
-def read():
-    return classification(get_data())
+        # Robot's variables
+        self.state = State()
+        self.pose_stamped = PoseStamped()
 
-def get_data():
-    data_list = [b'a', b'b', b'c', b'd' ,b'0', b'-', b'1',b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9']
-    #try:
-    data = hatpic.read(1)
-    start_timing = time.time()
-    while data == None:
-        data = hatpic.read(1)
-        #condition = True   # add tempo mesurement after 5 secondes on sort si pas de data
-        time.sleep(0.05)
-        t = time.time()
-        if t-start_timing > 2:
-            print('no data received: :(')
-            break
-    
-    if data:
-        if (isinstance(data, bytes)):
-            if (data.decode('utf-8')) == "i":
-                trame =  "i"
-                while (data.decode('utf-8')) != "o":
-                    data = hatpic.read(1)
-                    if (isinstance(data, bytes)):
-                        if data in data_list:
-                            trame = trame + data.decode('utf-8')
+        self.wrench_sub = rospy.Subscriber("/wrench_estimation", WrenchStamped, self.wrench_feedback_callback)
+
+        self.cmd_robot_pub = rospy.Publisher('mavros/setpoint_position/local', PoseStamped, queue_size=1)
+
+        self.timer_period = 0.033  # 30 Hz
+        # Timer definition
+        self.timer = rospy.Timer(rospy.Duration(self.timer_period), self.cmd_robot)
+
+    def write(self, x):
+        self.hatpic_ser.write(bytes(x,'utf-8'))
+        time.sleep(0.01)
+
+    def read(self):
+        return self.classification(self.get_data())
+
+    def get_data(self):
+        data_list = [b'a', b'b', b'c', b'd' ,b'0', b'-', b'1',b'2', b'3', b'4', b'5', b'6', b'7', b'8', b'9']
+        #try:
+        data = self.hatpic_ser.read(1)
+        start_timing = time.time()
+        while data == None:
+            data = self.hatpic_ser.read(1)
+            time.sleep(0.01)
+            t = time.time()
+            if t-start_timing > 2:
+                print('no data received: :(')
+                break
+            
+        if data:
+            if (isinstance(data, bytes)):
+                if (data.decode('utf-8')) == "i":
+                    data_frame =  "i"
+                    while (data.decode('utf-8')) != "o":
+                        data = self.hatpic_ser.read(1)
+                        if (isinstance(data, bytes)):
+                            if data in data_list:
+                                data_frame = data_frame + data.decode('utf-8')
+                            else:
+                                pass
                         else:
                             pass
-                    else:
-                        pass
-                
-                trame = trame +"o"
-                return trame
-    #except:
-    #    print("Error 1: get_data")
-    #    return None
+                        
+                    data_frame = data_frame +"o"
+                    return data_frame
+
+    def extraction_value(self, chain, start, end):
+        #print(chain)
+        start_index = chain.find(start)
+        end_index   = chain.find(end)
+        if start_index != -1 and end_index != -1:
+            return chain[start_index + len(start):end_index]
+        else:
+            return -1
+
+    def classification(self, data_frame):
+        if data_frame != None:
+            if data_frame[0:2] == 'ia':
+                self.data_a = int(self.extraction_value(data_frame, 'a', 'b'))
+                self.data_b = int(self.extraction_value(data_frame, 'b', 'c'))
+                self.data_c = int(self.extraction_value(data_frame, 'c', 'd'))
+                self.data_d = int(self.extraction_value(data_frame, 'd', 'o'))
+                data_hatpic = [self.data_a, self.data_b, self.data_c, self.data_d]
+                return data_hatpic
     
-def extraction_value(chaine, debut, fin):
-    #print(chaine)
-    debut_index = chaine.find(debut)
-    fin_index   = chaine.find(fin)
-    if debut_index != -1 and fin_index != -1:
-        return chaine[debut_index + len(debut):fin_index]
-    else:
-        return -1
+    def wrench_feedback_callback(self, msg):
+        data_joy_a = msg.wrench.force.x
+        data_joy_b = msg.wrench.force.y
+        data_joy_c = msg.wrench.force.z
+        data_joy_d = msg.wrench.torque.z
+        
+        data_joy_a *= 10
+        data_joy_a += 1000
+        if data_joy_a > 2000:
+            data_joy_a = 2000
+        elif data_joy_a < 0:
+            data_joy_a = 0
 
-def classification(trame):
-    #print(trame)
-    #global data_a, data_b, data_c, data_d
-    if trame != None:
-        if trame[0:2] == 'ia':
-            data_a = int(extraction_value(trame, 'a', 'b'))
-            data_b = int(extraction_value(trame, 'b', 'c'))
-            data_c = int(extraction_value(trame, 'c', 'd'))
-            data_d = int(extraction_value(trame, 'd', 'o'))
-            data_hatpic = [data_a, data_b, data_c, data_d]
-            return data_hatpic
+        print(data_joy_a)
+        data_joy_a = 1000
+        self.write('ia'+ str(data_joy_a) +'b2000c0d23o')
+        #try:
+        #    self.lin_vel_B = np.array([msg.twist.twist.linear.x,
+        #                               msg.twist.twist.linear.y,
+        #                               msg.twist.twist.linear.z])
+        #    self.ang_vel_B = np.array([msg.twist.twist.angular.x,
+        #                               msg.twist.twist.angular.y,
+        #                               msg.twist.twist.angular.z])
+        #except ValueError:
+        #    pass
 
-while True:
-    value  = write('ia100b2000c0d23o')
-    #value  = write('10')
-    #time.sleep(0.)
-    data2 = read()
-    if data2 != None:
-        print(data2)
+    def joystick_processing(self, data):
+        data -= 1000
+        if (data > self.dead_zone) or (data < -self.dead_zone):
+            return data * self.k_j
+        else:
+            return 0
+
+    def cmd_robot(self, event):
+        datas = self.read()
+        if datas != None:
+            x = self.joystick_processing(datas[0])
+            self.pose_stamped.pose.position.x += x
+            self.pose_stamped.pose.position.y = 0
+            self.pose_stamped.pose.position.z = 1
+
+            self.pose_stamped.pose.orientation.x = 0
+            self.pose_stamped.pose.orientation.y = 0
+            self.pose_stamped.pose.orientation.z = 0
+            self.pose_stamped.pose.orientation.w = 1
+
+            self.cmd_robot_pub.publish(self.pose_stamped)
+            #print(x)
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('hatpic_node', anonymous=True)
+
+        hatpic = Hatpic()
+        rospy.spin()
+        
+    except rospy.ROSInterruptException:
+        pass
